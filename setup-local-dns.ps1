f8bf3040f6fbf531d854394a8dd7d17a0f187099
felipe.nascimento <thrash.nation@gmail.com>
2018-01-17 15:33:16 +0000

    Updating dns setup script

--- setup-local-dns.ps1
+++ setup-local-dns.ps1
@@ -1,14 +1,23 @@
+#Reset param to remove the DNS restoring whatever is configured
+param([switch]$reset)
+
 #Select all IPv4 interfaces that have a *192* IP address
 $interfacesToUpdate = Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object {$_.ServerAddresses -like "*192*"}
 
 foreach($i in $interfacesToUpdate){
     echo "DNS Servers for $($i.InterfaceAlias) are : $($i.ServerAddresses -join ', ')" 
-    $newDnsServers = @("127.0.0.2")    
+    if($reset -eq $true){
+        $newDnsServers = @()    
+    }else{
+        $newDnsServers = @("127.0.0.2")    
+    }
     $externalAddresses = $i.ServerAddresses | Where-Object {$_ -notlike "*127*"}
     $externalAddresses.ForEach({$newDnsServers += $_})
     echo "New DNS Servers for $($i.InterfaceAlias) are : $($newDnsServers -join ', ')" 
-    Set-DNSClientServerAddress –interfaceIndex $i.InterfaceIndex –ServerAddresses $newDnsServers
+    Set-DNSClientServerAddress -interfaceIndex $i.InterfaceIndex -ServerAddresses $newDnsServers
 }
 
 #Do a ping to a expected DNS resolved domain
-ping test.ping
+if($reset -eq $false){
+    ping test.ping
+}
